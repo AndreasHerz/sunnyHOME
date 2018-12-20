ab2326fece2837a86cf5292de6d839806af6791e
Andreas Herz <43435739+AndreasHerz@users.noreply.github.com>
2018-12-20 13:30:25 +0100

    Thursday Update

--- Demos/RTC/TimeToDisplay/src/main.cpp
+++ Demos/RTC/TimeToDisplay/src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
+#include <wire.h>
+#include "OLED/SSD06.h"
+#include <time.h>
+
+
+SSD1306 display(0x3c, 21, 22);
+
 
 void setup() {
-  // put your setup code here, to run once:
+  display.init();
+
+  display.drawString(0, 0, "Hello World");
+  display.display();
 }
 
 void loop() {
